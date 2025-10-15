3772ff474d25a7c00473da8f3dcea1e34099191a
Elaine Ku <elaine.ku@zoom.us>
2025-10-15 14:06:15 -0700

    v0.0.1

--- Sources/SampleUIObjC/Internal/InSessionUI/Storage/SimulateStorage.h
+++ Sources/SampleUIObjC/Internal/InSessionUI/Storage/SimulateStorage.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+@import ZoomVideoSDK;
 #import "InSessionUI/Feedback/FeedbackPopViewController.h"
 #import "InSessionUI/Feedback/FeedbackSurveyResultTableViewCell.h"
 
