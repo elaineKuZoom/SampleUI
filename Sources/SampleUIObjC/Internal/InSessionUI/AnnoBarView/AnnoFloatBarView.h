8de81af330d3ca5072741d13f305f9ef0c05adc0
Elaine Ku <elaine.ku@zoom.us>
2025-10-15 14:57:20 -0700

    v0.0.1

--- Sources/SampleUIObjC/Internal/InSessionUI/AnnoBarView/AnnoFloatBarView.h
+++ Sources/SampleUIObjC/Internal/InSessionUI/AnnoBarView/AnnoFloatBarView.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+@import ZoomVideoSDK;
+#import "SampleUI.h"
 
 @interface AnnoFloatBarView : UIView
 - (instancetype)initWithAnnoHelper:(ZoomVideoSDKAnnotationHelper *)helper;
