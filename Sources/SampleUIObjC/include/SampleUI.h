515bd4f8bad9b161304c8ca454910abdafbe7c6c
Elaine Ku <elaine.ku@zoom.us>
2025-10-15 13:47:15 -0700

    v0.0.1

--- Sources/SampleUIObjC/include/SampleUI.h
+++ Sources/SampleUIObjC/include/SampleUI.h
@@ -1,6 +1,10 @@
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 @import ZoomVideoSDK;
+
+// Common macros
+#define RGBCOLOR(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
 
 FOUNDATION_EXPORT double SampleUIVersionNumber;
 FOUNDATION_EXPORT const unsigned char SampleUIVersionString[];
